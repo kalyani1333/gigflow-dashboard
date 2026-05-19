fb0460c4336fec1d5f718cf9c094d89a810a31d0
MKALYANI33 <230101120003@centurionuniv.edu.in>
2026-05-19 08:18:15 +0530

    fixing

--- frontend/src/services/leadService.ts
+++ frontend/src/services/leadService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL =
-  'http://localhost:5000/api/leads';
+'https://gigflow-dashboard-zt8h.onrender.com/api/leads';
 
 export const getLeads = async (
   page = 1
